558518452024a28206b298884365bd586da52c1e
unknown <jithinjithuedpl922@gmail.com>
2026-02-18 22:32:25 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
-    port: 5173,
+    
     strictPort: true,
-    allowedHosts: [
-      '974e-2403-a080-832-9d54-14cd-de07-c7b1-dd3c.ngrok-free.app'
-    ]
+    
   }
 })
